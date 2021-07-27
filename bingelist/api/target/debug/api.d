44f356b1ebe9ab57f98fa4d232832db5797298b5
Jess Nguyen <nguyjess@gmail.com>
2021-07-26 23:28:16 -0400

    rust build and rearrange files and folders

--- bingelist/api/target/debug/api.d
+++
@@ -1,1 +0,0 @@
-C:\Users\JS\Documents\github\lup-Binge-List\bingelist\api\target\debug\api.exe: C:\Users\JS\Documents\github\lup-Binge-List\bingelist\api\src\main.rs
--- bingelist/api/target/debug/deps/api-4c48f2a86082bad4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\JS\Documents\github\lup-Binge-List\bingelist\api\target\debug\deps\api-4c48f2a86082bad4.exe: src\main.rs
-
-C:\Users\JS\Documents\github\lup-Binge-List\bingelist\api\target\debug\deps\api-4c48f2a86082bad4.d: src\main.rs
-
-src\main.rs:
