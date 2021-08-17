98e5336e6d9b012501cc53a1a971d4d77cb5b07c
Jess Nguyen <nguyjess@gmail.com>
2021-08-16 21:21:38 -0400

    added bootstrap and added basic nav bar

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -1,11 +1,42 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/browse">Browse</router-link> |
-    <router-link to="/stats">Stats</router-link> |
-    <router-link to="/forum">Forum</router-link>
-  </div>
+<div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid" id="nav">
+      <a class="navbar-brand" href="#">BingeList</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/browse" class="nav-link">Browse</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/stats" class="nav-link">Stats</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/forum" class="nav-link">Forum</router-link>
+          </li>
+        </ul>
+      </div>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      <div >
+        <button class="btn btn-outline-success">Login</button>
+        <button class="btn btn-outline-primary">Register</button>
+      </div>
+      <div>
+        <button class="btn btn-outline-danger">Logout</button>
+      </div>
+    </div>
+  </nav>
   <router-view />
+</div>
 </template>
 
 <style lang="scss">
@@ -30,3 +61,12 @@
   }
 }
 </style>
+
+<script>
+// @ is an alias to /src
+// import Login from "@/components/Login.vue";
+
+export default{
+  name: "Home",
+};
+</script>
--- webui/src/views/Home.vue
+++ webui/src/views/Home.vue
@@ -7,7 +7,8 @@
 <script>
 // @ is an alias to /src
 import ActivityList from "@/components/ActivityList.vue";
-
+// import Vue from "vue";
+// import App from './app.vue';
 export default {
   name: "Home",
   components: {
