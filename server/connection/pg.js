7c6984c0543849d8093b9e343280d24873d0959f
Jess Nguyen <nguyjess@gmail.com>
2021-09-03 22:25:14 -0400

    show pg connection

--- server/connection/pg.js
+++ server/connection/pg.js
@@ -2,7 +2,9 @@ const { Client } = require("pg");
 require("dotenv").config();
 
 const client = new Client({
-  connectionString: process.env.CONNECTION,
+  connectionString:
+    process.env.CONNECTION ||
+    "postgres://pesknufeycrpxe:d12599ab2adcfda2d509568ab447805fa24ba3affca11ac8f0d3d60ecccef8a5@ec2-3-233-100-43.compute-1.amazonaws.com:5432/dema24mn6r1t6r",
   ssl: {
     rejectUnauthorized: false,
   },
