2cb6a2ed83fed14e3de73be83d82462ac26dc745
Jess Nguyen <nguyjess@gmail.com>
2021-09-11 11:58:47 -0400

    rebuild and removed static json

--- webui/service/api.factory.js
+++ webui/service/api.factory.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default {
   get dataApi() {
     return axios.create({
+      baseURL: '',
       headers: {
         'Content-type': 'application/json',
       },
