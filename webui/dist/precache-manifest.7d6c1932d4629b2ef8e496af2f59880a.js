8877883ca20601ef110831e97ad5fea4a8ca2315
Jess Nguyen <nguyjess@gmail.com>
2021-09-11 12:28:55 -0400

    Revert "rebuild and remove base url"
    This reverts commit b3ecfb0b50bfa5bcb0aef38bc13ae733e536daf0.

---
+++ webui/dist/precache-manifest.7d6c1932d4629b2ef8e496af2f59880a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33699124c9025da5e1b",
+    "url": "/css/app.461a96a6.css"
+  },
+  {
+    "revision": "140438be495c1c4dac6f",
+    "url": "/css/chunk-vendors.ca9f45ce.css"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/fonts/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/fonts/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/fonts/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/fonts/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/fonts/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/fonts/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/fonts/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/fonts/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/fonts/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/fonts/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "d54aaf893ce2c63081dcbe67ec59a4c2",
+    "url": "/img/LogoBL.d54aaf89.png"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/img/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/img/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/img/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/img/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/img/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "81a6a6a0848bca8c12f9a39dc41bc978",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d33699124c9025da5e1b",
+    "url": "/js/app.a8fd1c98.js"
+  },
+  {
+    "revision": "c0b8fe8eefe828d17d1b",
+    "url": "/js/browse.1b64c8f4.js"
+  },
+  {
+    "revision": "140438be495c1c4dac6f",
+    "url": "/js/chunk-vendors.d15e61ca.js"
+  },
+  {
+    "revision": "752e21959ff1c806b6daf2a641912135",
+    "url": "/manifest.json"
+  }
+]);
