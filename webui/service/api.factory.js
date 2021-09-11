fff5f9d199899e978ee421277156a16a67f8078d
Jess Nguyen <nguyjess@gmail.com>
2021-09-11 11:51:00 -0400

    remove baseurl in axios

--- webui/service/api.factory.js
+++ webui/service/api.factory.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export default {
   get dataApi() {
     return axios.create({
-      baseURL: 'http://localhost:5000/',
       headers: {
         'Content-type': 'application/json',
       },
