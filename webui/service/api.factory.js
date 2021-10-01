d4efdac68691406a8cdcf0139ba5c281296fa701
Jess Nguyen <nguyjess@gmail.com>
2021-09-30 23:44:17 -0400

    remove baseurl for deployment

--- webui/service/api.factory.js
+++ webui/service/api.factory.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
-let API_URL = 'https://bingelist.herokuapp.com';
-if (window.location.hostname === 'localhost') {
-  API_URL = 'http://localhost:8000';
-}
+// let API_URL = 'https://bingelist.herokuapp.com';
+// if (window.location.hostname === 'localhost') {
+//   API_URL = 'http://localhost:8000';
+// }
 
 export default {
   get dataApi() {
     return axios.create({
-      baseURL: API_URL,
       headers: {
         'content-type': 'application/json',
         authorization: `Bearer ${localStorage.token}`,
